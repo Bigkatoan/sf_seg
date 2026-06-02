1b9212075f419e9254ec731d28f40dd96875e9e3
bigkatoan <bigkatoan6969@gmail.com>
2026-06-02 03:48:43 +0000

    update

--- train.sh
+++ train.sh
@@ -2,4 +2,5 @@
 # Train sf_seg on COCO2017 person segmentation.
 # Usage: ./train.sh [extra args]
 # Example: ./train.sh --epochs 50 --loss-type combine
+rm -rf outputs checkpoints logs
 python train_sf_seg.py "$@"
