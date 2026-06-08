fa3bc94448119520d7364cd7326759fcbf345159
bigkatoan <bigkatoan6969@gmail.com>
2026-06-08 06:43:51 +0000

    update

--- train.sh
+++ train.sh
@@ -2,5 +2,5 @@
 # Train sf_seg on ADE20K-150 semantic segmentation.
 # Usage: ./train.sh [extra args]
 # Prepare data first: python -m src.dataloaders.ade20k --download
-rm -rf outputs checkpoints logs
+# rm -rf outputs checkpoints logs
 python -m src.training.trainer "$@"
